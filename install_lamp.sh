84b869ce395940f8354e4fb86afdfdc83e1badd6
TheFlyingCox <dandanthis@gmail.com>
2016-10-05 10:57:46 -0500

    Create install_lamp.sh

---
+++ install_lamp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=$1
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# install the LAMP stack
+apt-get -y install apache2 mysql-server php5 php5-mysql  
+
+# write some PHP
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
+echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
+
+# Pull over CHI Project Assets
+cd /var/www/
+wget -rkpN -e robots=off http://dev.cgicenterforhealth.org
+
+# restart Apache
+apachectl restart
