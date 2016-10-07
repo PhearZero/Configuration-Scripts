38b01d26e9dad4f488ce756c000746942e4eaebb
TheFlyingCox <dandanthis@gmail.com>
2016-10-06 21:52:38 -0500

    Create install_mysql.sh

---
+++ install_mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get -y update
+
+# set up a silent install of MySQL
+dbpass=$1
+
+export DEBIAN_FRONTEND=noninteractive
+echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
+echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
+
+# install the LAMP stack
+apt-get -y install mysql-server php5-mysql  
