8f35375beb5c5b6ca894b99dfaea1ab63a3190e5
TheFlyingCox <dandanthis@gmail.com>
2016-10-06 21:53:11 -0500

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 apt-get -y update
 
-# set up a silent install of MySQL
-dbpass=$1
-
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
 # install the LAMP stack
-apt-get -y install apache2 mysql-server php5 php5-mysql  
+apt-get -y install apache2 php5 php5-mysql  
 
 # write some PHP
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
