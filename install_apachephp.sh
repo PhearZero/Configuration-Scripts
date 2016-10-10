3ed6b03f78830e5306733135300a06e388290810
TheFlyingCox <dandanthis@gmail.com>
2016-10-10 18:29:33 -0500

    Update install_apachephp.sh
    Added Cron configuration.

--- install_apachephp.sh
+++ install_apachephp.sh
@@ -8,9 +8,13 @@ apt-get -y install apache2 php5 php5-mysql mysql-client
 echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\</center\> > /var/www/html/phpinfo.php
 echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
-# Pull over CHI Project Assets
-cd /var/www/html/
-wget -rkpN -e robots=off http://dev.cgicenterforhealth.org
+# Configure Cron to grab the latest default home page
+cat >> /var/spool/cron/root << EOD
+* * * * * curl -k https://cgilampdemo.blob.core.windows.net/html/index.html > /var/www/html/index.html
+EOD
+
+# Grab CHI Project Assets
+#wget -rkpN -e robots=off http://dev.cgicenterforhealth.org
 
 # restart Apache
 apachectl restart
