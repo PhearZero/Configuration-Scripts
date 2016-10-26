42fb3f8480a10370e4622691666509785f17ea6f
TheFlyingCox <dandanthis@gmail.com>
2016-10-26 12:39:13 -0500

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-apt-get -y update
+yum -y update
 
-# set up a silent install of MySQL
-dbpass=$1
+# Download and add the MySQL repository, then update.
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum -y update
 
-export DEBIAN_FRONTEND=noninteractive
-echo mysql-server-5.6 mysql-server/root_password password $dbpass | debconf-set-selections
-echo mysql-server-5.6 mysql-server/root_password_again password $dbpass | debconf-set-selections
-
-# install the LAMP stack
-apt-get -y install mysql-server php5-mysql  
+# Install MySQL and start service
+yum install -y mysql-server
+systemctl start mysqld
