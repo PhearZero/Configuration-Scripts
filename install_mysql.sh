f14c523b394f70d66b768308b16e27873386d762
TheFlyingCox <dandanthis@gmail.com>
2016-10-26 12:39:41 -0500

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -3,7 +3,7 @@ yum -y update
 
 # Download and add the MySQL repository, then update.
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
 yum -y update
 
 # Install MySQL and start service
