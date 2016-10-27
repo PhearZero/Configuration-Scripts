7d5b51b22666e031fe836cde54d998ca11fe66c8
TheFlyingCox <dandanthis@gmail.com>
2016-10-27 13:24:36 -0500

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-yum -y -q check-update
+#yum -y -q check-update
 
 # Download and add the MySQL repository, then update.
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum -y -q update
+#wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+#rpm -ivh mysql-community-release-el7-5.noarch.rpm
+#yum -y -q update
 
 # Install MySQL and start service
-yum install -y mysql-server
-systemctl start mysqld
+#yum install -y mysql-server
+#systemctl start mysqld
