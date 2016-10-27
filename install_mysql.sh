65827cce6817994b62a4f0c1211e38a406f8e5d2
TheFlyingCox <dandanthis@gmail.com>
2016-10-27 16:13:16 -0500

    Secured MySQL

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #yum -y -q check-update
 #yum -y -q update
+while getopts ":p:" o; do
+    case "${o}" in
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            exit;
+            ;;
+    esac
+done
 
 # Download and add the MySQL repository, then update.
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
@@ -8,4 +18,20 @@ rpm -ivh mysql-community-release-el7-5.noarch.rpm
 
 # Install MySQL and start service
 yum install -y mysql-server
+
+# Start MySQL Server
 systemctl start mysqld
+
+# Secure MySQL
+
+/usr/bin/mysql_secure_installation <<EOF
+
+y
+$p
+$p
+y
+y
+y
+y
+EOF
+
