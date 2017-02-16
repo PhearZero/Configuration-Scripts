ffd4108a04d15df9d775434150457bb05cb149ac
TheFlyingCox <dandanthis@gmail.com>
2017-02-16 13:26:52 -0600

    Update install_apachephp.sh

--- install_apachephp.sh
+++ install_apachephp.sh
@@ -9,12 +9,12 @@ yum -y install httpd php php-mysql mysql
 #echo \<\?php phpinfo\(\)\; \?\> >> /var/www/html/phpinfo.php
 
 # Configure Cron to grab the latest default home page
-#cat >> /var/spool/cron/root << EOD
-#* * * * * curl -k https://cgilampdemo.blob.core.windows.net/html/index.html > /var/www/html/index.html
-#EOD
+cat >> /var/spool/cron/root << EOD
+* * * * * curl -k https://cgilampdemo.blob.core.windows.net/html/index.html > /var/www/html/index.html
+EOD
 
 # Restart Cron service
-#service crond restart
+service crond restart
 
 # Grab CHI Project Assets
 #wget -rkpN -e robots=off http://dev.cgicenterforhealth.org
